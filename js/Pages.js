3e54390224650eedc43ec1309b18a9c55dcd71bc
ucudprogram <thegameconn@aol.com>
2016-04-17 20:52:02 +0000

    Setup of some logic for the Death Calc App

---
+++ js/Pages.js
@@ -0,0 +1,35 @@
+var pages = ["question", "prediction"];
+
+var ques1 = "What is your first Name";
+
+var ques2 = "What is your age";
+
+var ques3 = "What is your gender";
+
+var ques4 = "Where do you currently live";
+
+var ques5 = "What is your Marital Status";
+
+var ques6 = "What is your Ethnicity";
+
+var ques7 = "What Days of the week do you exercise";
+
+var ques8 = "What is your Height";
+
+var ques9 = "What is your Weight";
+
+
+var questions = [ ];
+var displayQuestions = function(){
+    
+};
+
+
+var calculateBMI = function(){
+    
+};
+
+var appStart = function(){
+    
+};
+document.addEventListener('DCOMContentLoaded', appStart);
