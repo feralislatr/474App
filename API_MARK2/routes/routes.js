54a77273e571bb988ed630442fb91019300e4471
rserva <rserva@udel.edu>
2016-05-17 14:31:07 +0000

    added some logic, the java version to be turned into node.js

--- API_MARK2/routes/routes.js
+++ API_MARK2/routes/routes.js
@@ -8,10 +8,52 @@ var appRouter = function(app) {
         MongoClient.connect(url, function(err,db) {
 	    assert.equal(null, err);
     	var collection = db.collection('tree');
-        //STUFF
+        // get attributes like so: req.query.[attribute]
+        //returns:
+        //var returnbody = {"attribute":"value"}
+        //return res.send(returnbody);
+        return res.send(answerFinder(collection, req, 0));
 	    db.close();
         });
     
     });
+}
+
+var answerFinder = function(collection, req, curID){
+   /* JAVA VERSION
+   if(n.isGoal()){
+			switch (n.goalCategory()){
+			case 1:
+				System.out.println("Accident");
+				break;
+			case 2:
+				System.out.println("Suicide");
+				break;
+			case 3:
+				System.out.println("Homicide");
+				break;
+			case 4:
+				System.out.println("Pending Investigation");
+				break;
+			case 5:
+				System.out.println("Could not determine");
+				break;
+			case 6:
+				System.out.println("Self-Inflicted");
+				break;
+			case 7:
+				System.out.println("Natural");
+				break;
+			}
+			return;
+		}
+		int feature = n.getFeature();
+		double splitval = n.getSplitVal();
+		if(d.getFeatures()[feature]<=splitval){
+			PrintAnswerHelper(n.getChildOne(),d);
+		}else{
+			PrintAnswerHelper(n.getChildTwo(),d);
+		}
+		*/
 }
 module.exports = appRouter;
