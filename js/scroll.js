0bcc7525dcbb5e821c5dfb6b27ba06a2ca976977
feralislatr <slate@udel.edu>
2016-05-17 12:15:45 +0000

    more front end stuff

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,9 @@
 //start with blank input value on load
-$(document).ready(function() {
+$(function() {
     $('input').val('');
 });
 
+//vertical scroll
 $(function() {
 	$('.move').bind('click',function(event){
 	    var $anchor = $(this);
@@ -15,3 +16,32 @@ $(function() {
 	});
 });
 
+
+
+//horizontal scroll
+// $(function() {
+//     $("#scroll").click(function(e) {
+//       console.log("horizontalscroll");
+//         e.preventDefault();
+//         $('#page2').animate({
+//             scrollLeft: $('#page2').scrollLeft() + $(this.hash).offset().left
+//         }, 500);
+//     });
+// });
+
+function scroll(){
+    console.log("i work1");
+    $("#page1").animate({left:'-1000px'},10000,'easeInOutExpo');
+    console.log("i work");
+    //$("#container").animate({'margin-left':'200px'},500);
+    };
+
+
+//page change
+function handleHash(){
+  $(location.hash).removeClass('hidden');
+  $('body').html($(location.hash).html());
+  
+}
+$(window).on("hashchange", handleHash);
+  
